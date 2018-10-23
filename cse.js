5e2c117466ae7987409cb1bbac18635162607e51
apkpureinfo <36444974+apkpureinfo@users.noreply.github.com>
2018-10-23 10:31:19 +0800

    Create cse.js

---
+++ cse.js
@@ -0,0 +1,10 @@
+
+  (function() {
+    var cx = '001213380992900820096:6rto9etol6k';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
